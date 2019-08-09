1374f17dfa7eba90e32802e1a85591bb44adc656
Jim Kroon <jim.kroon@deptagency.com>
2019-08-09 21:43:14 +0200

    Added control via keyboard Updated xbox-smartglass-core-node version

--- www/app/app.js
+++ www/app/app.js
@@ -8,13 +8,75 @@ module.exports = {
     _sgClient: false,
 
     start: function(){
+
+        document.onkeydown = function(evt) {
+            evt = evt || window.event;
+            console.log('Pressed key:', evt.keyCode)
+
+            if(this._sgClient != false){
+
+                if(evt.keyCode == 72) {
+                    this._sgClient.getManager('system_input').sendCommand('nexus').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+
+                if(evt.keyCode == 38) {
+                    this._sgClient.getManager('system_input').sendCommand('up').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+                if(evt.keyCode == 37) {
+                    this._sgClient.getManager('system_input').sendCommand('left').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+                if(evt.keyCode == 39) {
+                    this._sgClient.getManager('system_input').sendCommand('right').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+                if(evt.keyCode == 40) {
+                    this._sgClient.getManager('system_input').sendCommand('down').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+
+                if(evt.keyCode == 13 || evt.keyCode ==65) {
+                    this._sgClient.getManager('system_input').sendCommand('a').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+
+                if(evt.keyCode == 8 || evt.keyCode ==66) {
+                    this._sgClient.getManager('system_input').sendCommand('b').then(function(){
+                        // Send button
+                    }, function(error){
+                        console.log(error)
+                    });
+                }
+            }
+        }.bind(this);
+
     },
 
     scan: function(){
         var sgClient = new Smartglass();
         document.getElementById('consoleList').innerHTML = 'Searching...';
 
-        sgClient.discovery(function(consoles){
+        sgClient.discovery().then(function(consoles){
             document.getElementById('consoleList').innerHTML = '';
 
             for(var xbox in consoles){
@@ -32,28 +94,30 @@ module.exports = {
 
                 console.log('No consoles found on the network')
             }
+        }).catch(function(error){
+            console.log('app.scan error:', error)
         });
     },
 
     connect: function(ip){
         console.log('Connecting to ip:', ip);
         this._sgClient = new Smartglass();
-        this._sgClient.connect('192.168.2.5', function(result){
-            if(result === true){
-                console.log('Xbox succesfully connected!');
-                this._sgClient.addManager('system_input', SystemInputChannel())
-                this._sgClient.addManager('system_media', SystemMediaChannel())
-                this._sgClient.addManager('tv_remote', TvRemoteChannel())
+        this._sgClient.connect('192.168.2.5').then(function(){
+            console.log('Xbox succesfully connected!');
+            this._sgClient.addManager('system_input', SystemInputChannel())
+            this._sgClient.addManager('system_media', SystemMediaChannel())
+            this._sgClient.addManager('tv_remote', TvRemoteChannel())
 
-                document.getElementById('connectionStatus').innerHTML = 'Connected'
+            document.getElementById('connectionStatus').innerHTML = 'Connected'
 
-                this._sgClient.on('_on_console_status', function(message, xbox, remote, smartglass){
-                    if(message.packet_decoded.protected_payload.apps[0] != undefined){
-                        document.getElementById('currentApp').innerHTML = message.packet_decoded.protected_payload.apps[0].aum_id
-                    }
-                })
-            }
-        }.bind(this));
+            this._sgClient.on('_on_console_status', function(message, xbox, remote, smartglass){
+                if(message.packet_decoded.protected_payload.apps[0] != undefined){
+                    document.getElementById('currentApp').innerHTML = message.packet_decoded.protected_payload.apps[0].aum_id
+                }
+            })
+        }.bind(this)).catch(function(error){
+            console.log('app.connect error:', error)
+        });;
     }
 
 }
