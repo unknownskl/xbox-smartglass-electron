af1d8f3886ea808f626c02ecdf205a1d84cde733
Jim Kroon <info@jimkroon.nl>
2020-06-21 00:11:07 +0200

    Load friends status Updated xbox-webapi package to 0.1.2

--- www/app/app.js
+++ www/app/app.js
@@ -20,6 +20,8 @@ module.exports = {
 
     _status: {},
 
+    _friends: {},
+
     start: function(){
 
         this.loadView('home')
@@ -288,7 +290,7 @@ module.exports = {
                     document.getElementById('userAccounts').innerHTML = html
 
                     // Auth ok..  Load friends..
-                    // this.loadFriends()
+                    this.loadFriends()
                 }.bind(this)).catch(function(error){
                     console.log('error fail_load_profile', error)
                 })
@@ -309,43 +311,135 @@ module.exports = {
 
     },
 
-    // loadFriends: function(){
-    //     var token_store = TokenStore(appStorage.path.substr(0, appStorage.path.length-11)+'.tokens.json')
-    //     var client = XboxApiClient(token_store)
-    //
-    //     console.log('token_store',token_store)
-    //
-    //     client.authenticate().then(function(user_info){
-    //         document.getElementById('friendAccounts').innerHTML = ''
-    //
-    //         client.provider('social').get('/users/me/people').then(function(friends){
-    //
-    //             for(let friend in friends.people){
-    //
-    //                 client.provider('profile').get_user_profile(friends.people[friend].xuid).then(function(data){
-    //
-    //                     var html = '<li class="list-group-item">'
-    //                         html += '<img class="img-circle media-object pull-left" id="user_avatar" src='+data.profileUsers[0].settings[1].value+' width="32" height="32">'
-    //                         html += '  <div class="media-body">'
-    //                         html += '    <strong>'+data.profileUsers[0].settings[0].value+'</strong>'
-    //                         html += '    <p>User Online</p>'
-    //                         html += '  </div>'
-    //                         html += '</li>'
-    //
-    //                     document.getElementById('friendAccounts').innerHTML = document.getElementById('friendAccounts').innerHTML+html
-    //
-    //                 }.bind(this)).catch(function(error){
-    //                     console.log('error', error)
-    //                 })
-    //
-    //             }
-    //         }).catch(function(error){
-    //             console.log('error', error)
-    //         })
-    //     }).catch(function(error){
-    //         console.log(error)
-    //     })
-    // },
+    loadFriends: function(){
+        var token_store = TokenStore(appStorage.path.substr(0, appStorage.path.length-11)+'.tokens.json')
+        var client = XboxApiClient(token_store)
+    
+        console.log('token_store',token_store)
+    
+        client.authenticate().then(function(user_info){
+            document.getElementById('onlineFriendAccounts').innerHTML = ''
+            document.getElementById('offlineFriendAccounts').innerHTML = ''
+    
+            client.provider('profile').get_friends_by_xuid(client.auth_manager.xsts_token.DisplayClaims.xui[0].xid).then(function(friends){
+
+                // console.log(friends)
+
+                this._friends = {}
+
+                for(let friend in friends.profileUsers){
+                    this._friends[friends.profileUsers[friend].id] = {}
+
+                    for(let setting in friends.profileUsers[friend].settings){
+                        
+                        this._friends[friends.profileUsers[friend].id][
+                            friends.profileUsers[friend].settings[setting].id
+                        ] = friends.profileUsers[friend].settings[setting].value
+                    }
+                }
+
+                // Load presence...
+
+                var xuid_list = []
+                for(let xuid in this._friends){
+                    xuid_list.push(xuid)
+                }
+
+                client.provider('userpresence').get_presence_batch(xuid_list).then(function(data){
+                    console.log('presence: ', data)
+
+                    for(let entry in data){
+                        
+                        if(this._friends[data[entry].xuid] != undefined){
+                            
+                            this._friends[data[entry].xuid].Presence = data[entry].state,
+                            this._friends[data[entry].xuid].lastSeen = data[entry].lastSeen,
+                            this._friends[data[entry].xuid].Devices = data[entry].devices
+                        }
+                    }
+
+                    // Draw friends
+                    friends = this._friends
+                    for(let xuid in friends){
+        
+                        //client.provider('profile').get_user_profile(xuid).then(function(data){
+                        // console.log(friends[xuid])
+
+                        var html = '<li class="list-group-item">'
+                            html += '<img class="img-circle media-object pull-left" id="user_avatar" src='+friends[xuid].PublicGamerpic+' width="32" height="32">'
+                            html += '  <div class="media-body">'
+                            html += '    <strong>'+friends[xuid].Gamertag+'</strong>'
+                            if(friends[xuid].lastSeen != undefined){
+
+                                var lastseen = new Date(friends[xuid].lastSeen.timestamp)
+                                var now = new Date()
+                                
+                                html += '    <p>'+friends[xuid].Presence+' - Last seen: '+friends[xuid].lastSeen.titleName+' on '+friends[xuid].lastSeen.deviceType+'</p>'
+                                
+                                var diffHours = (((now-lastseen)/1000)/60/60)
+                                if(diffHours < 1){
+                                    var minutes = diffHours*60
+                                    html += '    <p>'+Math.floor(minutes)+' minutes ago</p>'
+                                } else if(diffHours > 24) {
+                                    var days = diffHours/24
+                                    html += '    <p>'+Math.floor(days)+' days ago</p>'
+                                } else {
+                                    html += '    <p>'+Math.floor(diffHours)+' hours ago</p>'
+                                }
+                            } else {
+                                html += '    <p>'+friends[xuid].Presence+'</p>'
+                            }
+
+                            if(friends[xuid].Devices != undefined){
+                                for(let device in friends[xuid].Devices){
+                                    if(friends[xuid].Devices[device].type == 'XboxOne'){
+                                        var devicetype = 'XboxOne'
+
+                                    } else if(friends[xuid].Devices[device].type == 'WindowsOneCore'){
+                                        var devicetype = 'PC'
+                                    } else {
+                                        var devicetype = friends[xuid].Devices[device].type
+                                    }
+
+                                    for(let title in friends[xuid].Devices[device].titles){
+                                        if(friends[xuid].Devices[device].titles[title].placement == 'Full'){
+                                            html += '    <p>'+devicetype+' - '+friends[xuid].Devices[device].titles[title].name+'</p>'
+
+                                            if(friends[xuid].Devices[device].titles[title].activity != undefined && friends[xuid].Devices[device].titles[title].activity.richPresence != undefined){
+                                                html += '    <p>'+friends[xuid].Devices[device].titles[title].activity.richPresence+'</p>'
+                                            }
+                                        }
+                                    }
+
+                                    // html += '    <p>XboxOne - '+JSON.stringify(friends[xuid].Devices[device])+'</p>'
+                                }
+                            }
+
+                            html += '  </div>'
+                            html += '</li>'
+
+                        if(friends[xuid].Presence == 'Offline')
+                            document.getElementById('offlineFriendAccounts').innerHTML = document.getElementById('offlineFriendAccounts').innerHTML+html
+                        else
+                            document.getElementById('onlineFriendAccounts').innerHTML = document.getElementById('onlineFriendAccounts').innerHTML+html
+        
+                        // }.bind(this)).catch(function(error){
+                        //     console.log('error', error)
+                        // })
+        
+                    }
+
+                }).catch(function(error){
+                    console.log('error', error)
+                })
+
+            }).catch(function(error){
+                console.log('error', error)
+            })
+        }).catch(function(error){
+            console.log(error)
+        })
+    },
 
     loadView: function(view, callback){
         fetch('assets/pages/'+view+'.html')
